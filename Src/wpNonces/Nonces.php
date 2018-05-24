bb7ccd47392ee8d22b161cc47b7072c4d96ba9db
Suleiman Mamman A <suleimamman@gmail.com>
2018-05-24 10:32:07 +0100

    wp_verify_nonce to verify Nonces function created

--- Src/wpNonces/Nonces.php
+++ Src/wpNonces/Nonces.php
@@ -10,6 +10,21 @@ class Nonces{
 	*/
 	function wp_create_nonce( $action ) {
 		return substr(md5( $action ), -15, 10 );
-	}
+    }
+    /**
+	*Create a nonce
+	*@param string $nonce value of nonce Nonce.
+	*@return string $action Optional. Action value. Default value -1..
+	*@return boolean this function will return boolean 'true' if the nonces is valid, else return false.
+	*/
+	function wp_verify_nonce($nonce, $action=-1) {
+         $nonce_calc = substr( md5( $action ), -15, 10 );
+            if ( $nonce == $nonce_calc ) {
+                return true;
+            } else {
+                return false;
+            }
+    }
+    
 
 }
