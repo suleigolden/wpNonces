6e9470d657448cdd19c7d5d0f275970ad796b4e7
Suleiman Mamman A <suleimamman@gmail.com>
2018-05-24 09:43:42 +0100

    Wp create Nonce function created that return's generated token sstring

--- Src/wpNonces/Nonces.php
+++ Src/wpNonces/Nonces.php
@@ -2,11 +2,14 @@
 namespace WpNonces;
 
 class Nonces{
-
-    public static function wp_create_nonce()
-    {
-        return 'Hello Nonces!';
-    }
-
+	/**
+	*Create a nonce
+	*@param string $action value of nonce action.
+	*@return string $nonce The nonce.
+	*@return this function return's a nonce generated token string 
+	*/
+	function wp_create_nonce( $action ) {
+		return substr(md5( $action ), -15, 10 );
+	}
 
 }
