053c00948f1cc3c5a4c667743827edcd3ac67da6
Suleiman Mamman A <suleimamman@gmail.com>
2018-05-25 08:48:47 +0100

    some comment formated

--- tests/test.php
+++ tests/test.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 use WpNonces\Nonces;
 
-// Create an object of the class
+// Create an object of the WpNonces class
 $nonce_Action = new WpNonces();
 
 // $action = 'Upload_save_Student_record';
@@ -14,7 +14,7 @@ $newnonce = $nonce_Action->wp_create_nonce( 'action_'.$post->id );
 //Verifying a nonce
 $nonce_Action->wp_verify_nonce_field( 'delete-student_'.$post_id );
 
-//Verifying a nonce passed in some other context:
+//Verifying a nonce passed in some other context
 $complete_url->wp_check_admin_referer( $_REQUEST['my_nonce'], 'update-student_'.$post->ID );
 
 //Adding a nonce to a URL
