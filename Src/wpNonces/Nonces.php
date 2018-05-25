7913102ddc473b0ac1b428672f652c015efc4a08
Suleiman Mamman A <suleimamman@gmail.com>
2018-05-25 05:07:41 +0100

    function to add nonce to a form created

--- Src/wpNonces/Nonces.php
+++ Src/wpNonces/Nonces.php
@@ -5,14 +5,22 @@ class Nonces{
 
     /** @var string|int the action the nonce is created for */
     protected $action;
+    /** @var string|int the action the nonce is created for */
+    protected $name;
+    /** @var string|int the action the nonce is created for */
+    protected $referer;
+    /** @var string|int the action the nonce is created for */
+    protected $echo;
 	/**
      * Class constructor.
      *
      * @param injecting (passing) the dependencies
      */
-    public function __construct($action = -1)
-    {
+    public function __construct($action = -1, $name='_wpnonce', $referer=true, $echo=true){
         $this->action = $action;
+        $this->name=$name;
+        $this->referer=$referer;
+        $this->echo=$echo;
     
 	}
 
@@ -39,6 +47,19 @@ class Nonces{
                 return false;
             }
     }
+
+     /**
+     *Add nonce to a FORM
+     *@param $action action name the default value is -1
+     *@param $referer a boolean to determine whether the referer hidden form field should be created, the default value is true.
+     *@param $name nonce name the default value is _wpnonce.
+     *@param $echo  a boolean to determine if the Wordpress should echo. the nonce hidden field or not, the default value is true.
+     *@return A nonce hidden form field and also referer is set to true.
+    */
+    public static function get_wp_nonce_field($action, $name, $referer, $echo) {
+        return wp_nonce_field($action, $name, $referer, $echo);
+    }
+
     /**
 	*Add nonce to URL 
 	*@param string $URL URL to add the nonce created.
