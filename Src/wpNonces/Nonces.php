e70b609b7c02d95f5af56c3f53635e2631cdeda0
Suleiman Mamman A <suleimamman@gmail.com>
2018-05-25 05:58:14 +0100

    An alternative way to veru nonce function created and param text variable formated

--- Src/wpNonces/Nonces.php
+++ Src/wpNonces/Nonces.php
@@ -16,18 +16,19 @@ class Nonces{
      *
      * @param injecting (passing) the dependencies
      */
-    public function __construct($action = -1, $name='_wpnonce', $referer=true, $echo=true){
+    public function __construct($action = -1, $name='_wpnonce', $referer=true, $echo=true,$param_query='_wpnonce'){
         $this->action = $action;
         $this->name=$name;
         $this->referer=$referer;
         $this->echo=$echo;
+        $this->param_query=$param_query;
     
 	}
 
 	/**
 	*Create a nonce
 	*@param string $action value of nonce action.
-	*@return string $nonce The nonce.
+	*@param string $nonce The nonce.
 	*@return this function return's a nonce generated token string 
 	*/
 	function wp_create_nonce( $action ) {
@@ -36,7 +37,7 @@ class Nonces{
     /**
 	*Verify a nonce
 	*@param string $nonce value of nonce Nonce.
-	*@return string $action Optional. Action value. Default value -1..
+	*@param string $action Optional. Action value. Default value -1..
 	*@return boolean this function will return boolean 'true' if the nonces is valid, else return false.
 	*/
 	function wp_verify_nonce($nonce, $action=-1) {
@@ -63,12 +64,20 @@ class Nonces{
     /**
 	*Add nonce to URL 
 	*@param string $URL URL to add the nonce created.
-	*@return string $action Name to assigned to nonce created
+	*@param string $action Name to assigned to nonce created
 	*@return boolean this function to Add nonce to URL actions.
 	*/
 	function get_wp_nonce_url($URL, $action) {
          return wp_nonce_url($URL, $action);
     }
-    
-
+    /**
+	*Verify nonces alternativly 
+	*@param string $action name.
+	*@param $param_query Name of nonce that WordPress will check for in the $_REQUEST variable, and the default value is _wpnonce.
+	*@return this function return '1' if nonce <= 12hours and '2' if 12hours < nonce < 24hours or 'false' if the an nonce invalid.
+	*/
+	function wp_check_admin_referer($action, $param_query) {
+         return check_admin_referer($action, $param_query);
+    }
+ 
 }
